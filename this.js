58fd57d66e6fc0c924c4ff3ab4eb24ddd4560e4a
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-10-23 22:55:00 +0530

    this keyword in JS

---
+++ this.js
@@ -0,0 +1,27 @@
+/**
+ * @this keyword in JavaScript
+ * this keyword in JavaScript deep
+ * - In JavaScript, this depends on the context in which "this" is currently in.
+ * 💡 7 Things you should know about "this"
+    👉 Rule 1) function with new keyword ( this refers to the function object )
+    👉 Rule 2) call, apply, bind ( this refers to the obj passed to methods )
+    👉 Rule 3) method in object ( this refers to the object )
+    👉 Rule 4) simple function ( this refers to window object, undefined in strict mode )
+    👉 Rule 5) multiple rules ( Higher rule has given priority )
+    👉 Rule 6) arrow function ( inherits "this" from its outer function )
+    👉 Miscellaneous important things about "this"
+
+  What we are going to cover in this section?
+  👉 What should I care about "this" keyword in JavaScript?
+  👉 What are Scopes in JavaScript?
+  👉 Types of Scopes in JavaScript?
+    - Global Scope
+    - Function Scope
+    - Block Scope
+    - Local Scope
+    - Lexical Scope/ Static Scope
+  👉 What is lexical scope in JavaScript?
+  👉 Different ways to call functions in JavaScript?
+  👉 How does binding of "this" keyword works in JavaScript?
+
+*/
